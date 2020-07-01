889089a43654b0e92ec680fbd718974d49c93ceb
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-01 20:38:27 +0530

    Modified the menu items

--- src/com/training/javafx/Main.java
+++ src/com/training/javafx/Main.java
@@ -1,9 +1,15 @@
 package com.training.javafx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -18,7 +24,10 @@ public class Main extends Application {
 		FXMLLoader loader = new
 				FXMLLoader(getClass().getResource("app_layout.fxml"));
 		Pane rootNode = loader.load();
-		
+
+		MenuBar menuBar = createMenu();
+		rootNode.getChildren().addAll(menuBar);
+
 		Scene scene = new Scene(rootNode, 300, 275);
 
 		primaryStage.setScene(scene);
@@ -26,5 +35,43 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 
+	private MenuBar createMenu() {
+		//File menu
+		Menu fileMenu = new Menu("File");
+		MenuItem newMenuItem = new MenuItem("New");
+		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+		MenuItem quitMenuItem = new MenuItem("Quit");
+
+		quitMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+		fileMenu.getItems().addAll(newMenuItem,separatorMenuItem,quitMenuItem);
+
+		//Help menu
+		Menu helpMenu = new Menu("Help");
+		MenuItem aboutApp = new MenuItem("About");
+
+		aboutApp.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				aboutApp();
+			}
+		});
+		helpMenu.getItems().addAll(aboutApp);
+
+		//Menu bar
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu, helpMenu);
+
+		return menuBar;
+	}
+
+	private void aboutApp() {
+		//TODO
+	}
 
 }
